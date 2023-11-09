d77ed662259a3f5e35c1479c956853b65da03ab6
Ridho Khalis <ridho@transfez.com>
2023-11-09 13:38:19 +0700

    feat: add mysql2 and devise gems to Gemfile

--- Gemfile
+++ Gemfile
@@ -60,3 +60,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'mysql2', '>= 0.4.4', '< 0.6.0'
+
+gem 'devise', '~> 4.9', '>= 4.9.2'
---
+++ db/migrate/20231109063535_create_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :transactions do |t|
+      t.references :user, foreign_key: true
+      t.decimal :amount
+      t.references :category, foreign_key: true
+      t.text :description
+      t.date :transaction_date
+
+      t.timestamps
+    end
+  end
+end
